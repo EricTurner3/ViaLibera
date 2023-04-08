189da7cc1d631ca0c41bcedc2924a2fd68ab2c71
Eric Turner <me@ericturner.it>
2023-04-07 21:44:26 -0400

    vehicle view and sidebar re-do

--- inspection/apps.py
+++ inspection/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class InspectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'inspection'
+    icon = 'fas fa-magnifying-glass'
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
+from .models import Vehicle
 
 # Create your views here.
+@login_required
 def index(request):
+    vehicles = Vehicle.objects.all()
+    paginator = Paginator(vehicles, 10) # 10 vehicles per page
 
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     # Page from the theme 
-    return render(request, 'pages/dashboard.html')
+    return render(request, 'index.html', {'page_obj': page_obj})
